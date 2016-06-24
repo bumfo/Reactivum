10b3bd1487dade8210272b61098d461d6bfba0e2
bumfo <bumfod@gmail.com>
2016-06-25 07:30:45 +0800

    backup

--- unit.js
+++ unit.js
@@ -1,10 +1,11 @@
 const Reactivum = require('./reactivum.js');
 
 let demoVum = Reactivum({
-	input: '2',
+	input0: 1,
+	input: 2,
 	result: 3,
 }, '#demo');
 
 demoVum.$listen('input', () => {
-	demoVum.result = +demoVum.input + 1;
+	demoVum.result = +demoVum.input0 + +demoVum.input;
 });
